5d8311e55976111a8ca400fe1e50baf66961786e
Mark Patton <mark.patton@gmail.com>
2024-03-14 12:10:11 -0400

    Make pass.local munging more complete. Change sp login location

--- idp/entrypoint.sh
+++ idp/entrypoint.sh
@@ -3,6 +3,8 @@ echo "IDP_HOST: $IDP_HOST"
 echo "Updating metadata files..."
 sed -i "s#https://pass.local#$IDP_HOST#g" /opt/shibboleth-idp/metadata/idp-metadata.xml
 sed -i "s#https://pass.local#$IDP_HOST#g" /opt/shibboleth-idp/metadata/sp-metadata.xml
+sed -i "s#https://pass.local#$IDP_HOST#g" /opt/shibboleth-idp/conf/cas-protocol.xml
+sed -i "s#https://pass.local#$IDP_HOST#g" /opt/shibboleth-idp/conf/idp.properties
 
 echo "Starting Jetty"
 /usr/local/bin/run-jetty.sh
