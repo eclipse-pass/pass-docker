82cf273842354774f5b64dfb754d0e5b753c7cc4
Russ Poetker <rpoetke1@jh.edu>
2025-02-03 15:11:06 -0500

    Clean code

--- certs/cert-gen.sh
+++ certs/cert-gen.sh
@@ -17,6 +17,7 @@ PASS_BASE=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c15)
 KEY_PW="${PASS_BASE}keypw"
 STORE_PW="${PASS_BASE}localstorepw"
 P12STORE_PW="${PASS_BASE}pkcs12pw"
+
 keytool -genkeypair -alias localtest -keyalg RSA -dname $CERT_DNAME -keystore /cert-gen-output/localtest.jks \
   -keypass $KEY_PW -storepass $STORE_PW $CERT_EXT_ARG
 keytool -importkeystore -srckeystore /cert-gen-output/localtest.jks -srcstorepass $STORE_PW \
