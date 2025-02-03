9e0f7d4b6fe509fe43c0427deb6de10359c58aa7
Russ Poetker <rpoetke1@jh.edu>
2025-02-03 14:08:10 -0500

    Clean code

--- certs/cert-gen.sh
+++ certs/cert-gen.sh
@@ -8,14 +8,14 @@ KEY_PW=$3
 STORE_PW=$4
 P12STORE_PW=$5
 
-CERT_EXT_ARG=
+CERT_EXT_ARG=""
 
 if [ -n "$CERT_EXT" ];then
   CERT_EXT_ARG="-ext ${CERT_EXT}"
 fi
 
 keytool -genkeypair -alias localtest -keyalg RSA -dname $CERT_DNAME -keystore /cert-gen-output/localtest.jks \
-          -keypass $KEY_PW -storepass $STORE_PW $CERT_EXT_ARG
+  -keypass $KEY_PW -storepass $STORE_PW $CERT_EXT_ARG
 keytool -importkeystore -srckeystore /cert-gen-output/localtest.jks -srcstorepass $STORE_PW \
   -srckeypass $KEY_PW -srcalias localtest -destalias localtest \
   -destkeystore /cert-gen-output/localtest.p12 -deststoretype PKCS12 -deststorepass $P12STORE_PW \
