eb63c7238d7a8ca6e9c1ef9dc4470bad2e3614cf
Russ Poetker <rpoetke1@jh.edu>
2025-02-03 13:48:56 -0500

    Use cert-gen for inveniordm local server

--- invenio-rdm/build.sh
+++ invenio-rdm/build.sh
@@ -2,5 +2,7 @@
 # The invenio-cli python package must be installed for this script to run. https://inveniordm.docs.cern.ch/install/cli/
 
 cd ./pass-docker-invenio-rdm
+docker compose -f ../../certs/cert-gen-invenio.yml up
+docker compose -f ../../certs/cert-gen-invenio.yml down -v
 invenio-cli packages lock
 invenio-cli containers build
